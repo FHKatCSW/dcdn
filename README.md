5e56609d144fc269ebb8aab3b67f0076d8fb216d
FlorianHandke <34957612+FlorianHandke@users.noreply.github.com>
2022-03-31 10:49:55 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # dcdn
 Basic setup to run a dash webserver and nginx with docker-compose
+
+## Setup
+
+### Mac
+
+If your a Mac user you need the following steps to bring docker to life:
+
+Install CommandLine Tools
+
+    xcode-select --install
+
+Install uwsgi
+
+    pip install uwsgi
+
+Got to directory
+
+    cd <YOUR_FILE_LOCATION>
+
+Bring up Docker
+
+    docker-compose up --build
---
+++ web/app.py
@@ -0,0 +1,22 @@
+from dash import Dash, html, dcc
+import flask
+
+server = flask.Flask(__name__)
+app = Dash(server=server)
+
+markdown_text = '''
+### Dash and Markdown
+
+Dash apps can be written in Markdown.
+Dash uses the [CommonMark](http://commonmark.org/)
+specification of Markdown.
+Check out their [60 Second Markdown Tutorial](http://commonmark.org/help/)
+if this is your first introduction to Markdown!
+'''
+
+app.layout = html.Div([
+    dcc.Markdown(children=markdown_text)
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
